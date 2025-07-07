3ec64ef044ec4449958780927fea2265c1164a15
omateusamaral <mateus.amaral018@gmail.com>
2025-07-07 17:37:02 -0300

    first commit

---
+++ src/test/java/io/github/com/omateusamaral/todo_spring_boot/TodoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.com.omateusamaral.todo_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
